2037c352704e049001414ee6105306ccd5777f4f
sqw228 <dmitry.kuzmenko01@gmail.com>
2026-09-20 13:56:11 +0300

    Add startup logging

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import asyncio
+import logging
 
 from bot import run_bot
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
 
 
 if __name__ == "__main__":
